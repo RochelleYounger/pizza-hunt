a59a26f3f8cd9b4de7f1f6b2eaf2a94e2aa464ff
Rochelle Younger <regnuoy321@gmail.com>
2022-04-23 15:46:22 -0400

    added nested reply functionality

--- models/Comment.js
+++ models/Comment.js
@@ -1,5 +1,31 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
+
+const ReplySchema = new Schema(
+  {
+    // set custom id to avoid confusion with parent comment _id
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    writtenBy: {
+      type: String
+    },
+    replyBody: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
 
 const CommentSchema = new Schema(
   {
@@ -12,16 +38,23 @@ const CommentSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
-      // get: (createdAtVal) => dateFormat(createdAtVal) //may need later
-    }
+      get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    // use ReplySchema to validate data for a reply
+    replies: [ReplySchema]
   },
-  // {
-  //   toJSON: {
-  //     getters: true
-  //   },
-  //   id: false
-  // } //may need later
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
 );
+
+CommentSchema.virtual('replyCount').get(function() {
+  return this.replies.length;
+});
 
 const Comment = model('Comment', CommentSchema);
 
